b8b7105e5df402afcdd92b1bbf8417f14ae19d80
Luca Andrei <andrluc@c4b301d541b3.ant.amazon.com>
2017-11-10 23:15:25 +0200

    Finished the first version of the ANN homework 3

--- Tema-3/tema3.py
+++ Tema-3/tema3.py
@@ -1,8 +1,16 @@
 import cPickle, gzip, numpy, math
 
 
-def sigmoid(x):
-    return 1 / (1 + math.exp(-x))
+def cost_derivative(output_activations, y):
+    return output_activations - y
+
+
+def sigmoid(z):
+    return 1.0 / (1.0 + numpy.exp(-z))
+
+
+def sigmoid_prime(z):
+    return sigmoid(z) * (1.0 - sigmoid(z))
 
 
 array_sigmoid = numpy.vectorize(sigmoid)
@@ -10,13 +18,13 @@ array_sigmoid = numpy.vectorize(sigmoid)
 input_size = 784
 
 number_of_digits = 10
-nr_iterations = 10
-hidden_layer_size = 10
+nr_iterations = 1
+hidden_layer_size = 20
 learning_rate = 0.1
 number_of_layers = 2
 
-layer_weights = [numpy.random.normal(1 / 2, 1, (input_size, hidden_layer_size)),
-                 numpy.random.normal(1 / 2, 1, (hidden_layer_size, number_of_digits))]
+layer_weights = [numpy.random.normal(0, 1 / (math.sqrt(input_size)), (input_size, hidden_layer_size)),
+                 numpy.random.normal(0, 1 / (math.sqrt(hidden_layer_size)), (hidden_layer_size, number_of_digits))]
 biases = [numpy.zeros(hidden_layer_size), numpy.zeros(number_of_digits)]
 
 f = gzip.open('mnist.pkl.gz', 'rb')
@@ -25,15 +33,59 @@ f.close()
 
 for iteration in range(nr_iterations):
     for train_number in range(len(train_set[0])):
+        train_input = []
+        for i in train_set[0][train_number]:
+            train_input.append(i)
+
+        train_input = numpy.reshape(train_input, (784, 1))
+
         print str(iteration) + " " + str(train_number)
 
+        activation_layer_results = [numpy.zeros(hidden_layer_size), numpy.zeros(number_of_digits)]
         layer_results = [numpy.zeros(hidden_layer_size), numpy.zeros(number_of_digits)]
-        layer_results[0] = array_sigmoid(numpy.dot(train_set[0][train_number], layer_weights[0]) + biases[0])
+        layer_errors = [numpy.zeros(hidden_layer_size), numpy.zeros(number_of_digits)]
 
-        aux = numpy.exp(numpy.dot(layer_results[0], layer_weights[1]) + biases[1])
-        layer_results[1] = aux / sum(aux)
+        layer_results[0] = numpy.dot(train_set[0][train_number], layer_weights[0]) + biases[0]
+        activation_layer_results[0] = numpy.array(map(lambda x: sigmoid(x), layer_results[0]))
 
+        layer_results[1] = numpy.dot(activation_layer_results[0], layer_weights[1]) + biases[1]
+        activation_layer_results[1] = numpy.array(map(lambda x: sigmoid(x), layer_results[1]))
 
+        for digit in range(0, number_of_digits):
+            layer_errors[1][digit] = (activation_layer_results[1][digit] - float(int(train_set[1][train_number] == digit))) \
+                                                        * (1 - activation_layer_results[1][digit]) * activation_layer_results[1][digit]
 
+        layer_errors[0] = (1 - activation_layer_results[0]) * activation_layer_results[0] \
+                          * numpy.dot(layer_errors[1], layer_weights[1].transpose())
 
+        layer_errors_reshaped_1 = numpy.reshape(layer_errors[0], (hidden_layer_size, 1))
+        layer_errors_reshaped_2 = numpy.reshape(layer_errors[1], (number_of_digits, 1))
+        activation_layer_results_reshaped_1 = numpy.reshape(activation_layer_results[0], (hidden_layer_size, 1))
 
+        layer_weights[0] -= numpy.dot(train_input, numpy.transpose(layer_errors_reshaped_1)) * learning_rate
+        layer_weights[1] -= numpy.dot(activation_layer_results_reshaped_1, numpy.transpose(layer_errors_reshaped_2)) * learning_rate
+
+        biases[0] -= layer_errors[0] * learning_rate
+        biases[1] -= layer_errors[1] * learning_rate
+
+successes = 0
+fails = 0
+for i in range(0, len(test_set[0])):
+    activation_layer_results = [numpy.zeros(hidden_layer_size), numpy.zeros(number_of_digits)]
+    layer_results = [numpy.zeros(hidden_layer_size), numpy.zeros(number_of_digits)]
+
+    layer_results[0] = numpy.dot(test_set[0][i], layer_weights[0]) + biases[0]
+    activation_layer_results[0] = numpy.array(map(lambda x: sigmoid(x), layer_results[0]))
+    layer_results[1] = numpy.dot(activation_layer_results[0], layer_weights[1]) + biases[1]
+    activation_layer_results[1] = numpy.array(map(lambda x: sigmoid(x), layer_results[1]))
+
+    print numpy.argmax(activation_layer_results[1])
+
+    if test_set[1][i] == numpy.argmax(activation_layer_results[1]):
+        successes += 1
+    else:
+        fails += 1
+
+print "Successes: " + str(successes)
+print "Fails: " + str(fails)
+print str(float(successes) / float((successes + fails)) * 100) + "%"
