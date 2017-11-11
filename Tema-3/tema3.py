d32c5931049b890298cfae51beb4b864018b3936
Luca Andrei <andrluc@c4b301d541b3.ant.amazon.com>
2017-11-11 10:04:20 +0200

    Added regularization + softmax + momentum

--- Tema-3/tema3.py
+++ Tema-3/tema3.py
@@ -19,12 +19,17 @@ input_size = 784
 
 number_of_digits = 10
 nr_iterations = 1
-hidden_layer_size = 20
+hidden_layer_size = 100
 learning_rate = 0.1
 number_of_layers = 2
+regularization_rate = 0.1
+friction_rate = 0.1
 
 layer_weights = [numpy.random.normal(0, 1 / (math.sqrt(input_size)), (input_size, hidden_layer_size)),
                  numpy.random.normal(0, 1 / (math.sqrt(hidden_layer_size)), (hidden_layer_size, number_of_digits))]
+
+layer_weights_speed = [numpy.zeros((input_size, hidden_layer_size)), numpy.zeros((hidden_layer_size, number_of_digits))]
+
 biases = [numpy.zeros(hidden_layer_size), numpy.zeros(number_of_digits)]
 
 f = gzip.open('mnist.pkl.gz', 'rb')
@@ -51,9 +56,14 @@ for iteration in range(nr_iterations):
         layer_results[1] = numpy.dot(activation_layer_results[0], layer_weights[1]) + biases[1]
         activation_layer_results[1] = numpy.array(map(lambda x: sigmoid(x), layer_results[1]))
 
-        for digit in range(0, number_of_digits):
-            layer_errors[1][digit] = (activation_layer_results[1][digit] - float(int(train_set[1][train_number] == digit))) \
-                                                        * (1 - activation_layer_results[1][digit]) * activation_layer_results[1][digit]
+        # softmax last layer error
+        layer_errors[1] = numpy.exp(layer_results[1]) / numpy.sum(numpy.exp(layer_results[1]))
+        layer_errors[1][train_set[1][train_number]] -= 1
+
+        # classic sigmoid error
+        # for digit in range(0, number_of_digits):
+        #     layer_errors[1][digit] = (activation_layer_results[1][digit] - float(int(train_set[1][train_number] == digit))) \
+        #                                                 * (1 - activation_layer_results[1][digit]) * activation_layer_results[1][digit]
 
         layer_errors[0] = (1 - activation_layer_results[0]) * activation_layer_results[0] \
                           * numpy.dot(layer_errors[1], layer_weights[1].transpose())
@@ -62,8 +72,18 @@ for iteration in range(nr_iterations):
         layer_errors_reshaped_2 = numpy.reshape(layer_errors[1], (number_of_digits, 1))
         activation_layer_results_reshaped_1 = numpy.reshape(activation_layer_results[0], (hidden_layer_size, 1))
 
-        layer_weights[0] -= numpy.dot(train_input, numpy.transpose(layer_errors_reshaped_1)) * learning_rate
-        layer_weights[1] -= numpy.dot(activation_layer_results_reshaped_1, numpy.transpose(layer_errors_reshaped_2)) * learning_rate
+        # momentum
+        layer_weights_speed[0] = layer_weights_speed[0] * friction_rate - \
+                                 numpy.dot(train_input, numpy.transpose(layer_errors_reshaped_1)) * learning_rate
+        layer_weights_speed[1] = layer_weights_speed[1] * friction_rate -\
+                                 numpy.dot(activation_layer_results_reshaped_1, numpy.transpose(layer_errors_reshaped_2)) * learning_rate
+
+        # L2 regularization
+        layer_weights[0] *= (1 - learning_rate * (regularization_rate / len(layer_weights[0])))
+        layer_weights[1] *= (1 - learning_rate * (regularization_rate / len(layer_weights[1])))
+
+        layer_weights[0] += layer_weights_speed[0]
+        layer_weights[1] += layer_weights_speed[1]
 
         biases[0] -= layer_errors[0] * learning_rate
         biases[1] -= layer_errors[1] * learning_rate
